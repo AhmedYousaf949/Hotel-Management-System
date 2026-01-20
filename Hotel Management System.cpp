b4a2d9b2ee8bd808e56640d6b091a06b6a746b35
AhmedYousaf949 <f2024266949@umt.edu.pk>
2026-01-20 19:39:48 +0500

    Update Files

--- Hotel Management System.cpp
+++ Hotel Management System.cpp
@@ -96,7 +96,7 @@ void assignRoom() {
         }
         f = f->next;
     }
-    cout << "No rooms available\n";
+    cout << "No available rooms\n";
 }
 
 void undoBooking() {
@@ -131,33 +131,69 @@ void viewHistory() {
     }
 }
 
+void showAvailability() {
+    Floor* f = floorHead;
+    if (!f) {
+        cout << "No floors available\n";
+        return;
+    }
+    while (f) {
+        cout << "Floor " << f->floorNo << ": ";
+        Room* r = f->rooms;
+        while (r) {
+            if (!r->booked)
+                cout << r->roomNo << "(Free) ";
+            r = r->next;
+        }
+        cout << endl;
+        f = f->next;
+    }
+}
+
+void listAllRooms() {
+    Floor* f = floorHead;
+    if (!f) {
+        cout << "No floors available\n";
+        return;
+    }
+    while (f) {
+        cout << "Floor " << f->floorNo << ": ";
+        Room* r = f->rooms;
+        while (r) {
+            if (r->booked)
+                cout << r->roomNo << "(Booked) ";
+            else
+                cout << r->roomNo << "(Free) ";
+            r = r->next;
+        }
+        cout << endl;
+        f = f->next;
+    }
+}
+
 void adminMenu() {
     int c;
     do {
-        cout << "\n1 AddFloor 2 AddRoom 3 History 4 Undo 0 Logout: ";
+        cout << "\n1 AddFloor 2 AddRoom 3 Availability 4 AllRooms 5 History 6 Undo 0 Logout: ";
         cin >> c;
-        if (c == 1) {
-            int f;
-            cin >> f;
-            addFloor(f);
-        }
-        else if (c == 2) {
-            int f, r;
-            cin >> f >> r;
-            addRoom(f, r);
-        }
-        else if (c == 3) viewHistory();
-        else if (c == 4) undoBooking();
+        if (c == 1) { int f; cin >> f; addFloor(f); }
+        else if (c == 2) { int f, r; cin >> f >> r; addRoom(f, r); }
+        else if (c == 3) showAvailability();
+        else if (c == 4) listAllRooms();
+        else if (c == 5) viewHistory();
+        else if (c == 6) undoBooking();
     } while (c);
 }
 
 void employeeMenu() {
     int c;
     do {
-        cout << "\n1 AssignRoom 2 Undo 0 Logout: ";
+        cout << "\n1 AssignRoom 2 Availability 3 AllRooms 4 Undo 0 Logout: ";
         cin >> c;
         if (c == 1) assignRoom();
-        else if (c == 2) undoBooking();
+        else if (c == 2) showAvailability();
+        else if (c == 3) listAllRooms();
+        else if (c == 4) undoBooking();
     } while (c);
 }
 
@@ -168,15 +204,12 @@ void adminEmpLogin() {
     cout << "Password: ";
     cin >> pass;
 
-    if (match(user, (char*)"admin") && match(pass, (char*)"123")) {
+    if (match(user, (char*)"admin") && match(pass, (char*)"123"))
         adminMenu();
-    }
-    else if (match(user, (char*)"emp") && match(pass, (char*)"123")) {
+    else if (match(user, (char*)"emp") && match(pass, (char*)"123"))
         employeeMenu();
-    }
-    else {
+    else
         cout << "Invalid login\n";
-    }
 }
 
 void customerMenu() {
